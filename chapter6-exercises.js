2a62833fce00668a0ec5f1b2dd93c4242af0304c
Julyo-Hidalgo <julyohidalgo0@gmail.com>
2025-08-23 23:02:47 -0300

    feat: Add first two exercises of chapter 6

---
+++ chapter6-exercises.js
@@ -0,0 +1,53 @@
+//a vector type
+class Vec{
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	plus(Vec2){
+		return (new Vec(this.x + Vec2.x, this.y + Vec2.y));
+	}
+
+	minus(Vec2){
+		return (new Vec(this.x - Vec2.x, this.y - Vec2.y));
+	}
+
+	get length(){
+		return (Math.sqrt(this.x*this.x + this.y*this.y));
+	}
+}
+
+//groups
+class Group{
+	constructor(){
+		this.elements = [];
+	}
+
+	add(value){
+		if(this.elements.indexOf(value) == -1)
+			this.elements.push(value);
+	}
+
+	delete(value){
+		let index = this.elements.indexOf(value);
+		if(index != -1)
+			this.elements = this.elements.slice(0, index).concat(this.elements.slice(index + 1));
+	}
+
+	has(value){
+		if(this.elements.indexOf(value) != -1)
+			return true;
+
+		return false;
+	}
+
+	static from(values){
+		let group = new Group();
+
+		for (let value of values)
+			group.add(value);
+
+		return group;
+	}
+}
